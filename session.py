b7cb4830e22d62e722f00fccec9c2ed8b20120eb
davevj3907-oss <davevj3907@gmail.com>
2026-08-28 22:11:43 +0530

    Add files via upload

---
+++ session.py
@@ -0,0 +1,51 @@
+#1. Flipkart Product
+"""product_name = "iPhone 15"
+price = 69999
+is_available = True
+rating = 4.5
+
+print(product_name, type(product_name))
+print(price, type(price))
+print(is_available, type(is_available))
+print(rating, type(rating))"""
+
+#2. Zomato Restaurant
+"""restaurant_name = "Food Palace"
+cuisine_type = "Gujarati"
+average_cost = 500
+is_open_now = True
+
+print(restaurant_name)
+print(cuisine_type)
+print(average_cost)
+print(is_open_now)"""
+
+#3. Spotify Playlist — id()
+"""playlist_title = "My Favorites"
+number_of_songs = 25
+is_public = True
+
+print("Playlist:", playlist_title)
+print("Memory address:", id(playlist_title))
+
+print("Number of songs:", number_of_songs)
+print("Memory address:", id(number_of_songs))
+
+print("Is public:", is_public)
+print("Memory address:", id(is_public))"""
+
+#4. YouTube Video — Primitive & Non-Primitive
+title = "Python Tutorial"
+views = 100000
+likes = 5000
+is_premium = True
+
+# title is a primitive/basic data type: string (str)
+# views is a primitive/basic data type: integer (int)
+# likes is a primitive/basic data type: integer (int)
+# is_premium is a primitive/basic data type: boolean (bool)
+
+print(title)
+print(views)
+print(likes)
+print(is_premium)
